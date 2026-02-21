ee36bfb5ad45b760cc9e178dcb27a2cc3a1ee3be
Valerie Warner <valerie.warner@ink.fish>
2026-02-21 15:30:00 +0000

    replace theme symlink with managed tokyo night plugin spec

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,13 @@
-/home/warnerva/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
